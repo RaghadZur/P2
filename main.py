8ec54d9ba79d31ebbf82993a14502471b3d736e7
Raghad <72172564+RaghadZur@users.noreply.github.com>
2022-01-30 23:18:06 +0000

    importing needed libraries

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import torch
+import matplotlib.pyplot as plt # for plotting
+import numpy as np # for transformation
 
-print(torch.__version__)
+import torch # PyTorch package
+import torchvision # load datasets
+import torchvision.transforms as transforms # transform data
+import torch.nn as nn # basic building block for neural neteorks
+import torch.nn.functional as F # import convolution functions like Relu
+import torch.optim as optim # optimzer
