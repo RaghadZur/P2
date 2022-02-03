18334000530c85e24ada02f7976e136c7642168e
Raghad <72172564+RaghadZur@users.noreply.github.com>
2022-02-03 23:13:33 +0000

    draft code completed

--- 2 - Neural Networks Tutorial.py
+++ 2 - Neural Networks Tutorial.py
@@ -10,7 +10,7 @@ import torchvision.transforms as transforms
 
 # %%%%%%%%%%%%%%%%% CREATING FULLY CONNECTED NEURAL NETWORKS %%%%%%%%%%%%%%%
 class NN(nn.Module):
-    def __init__(self, input_size, num_classes): # size = 784 (28x8x)
+    def __init__(self, input_size, num_classes):  # size = 784 (28x8x)
         super(NN, self).__init__()
         self.fc1 = nn.Linear(input_size, 50)
         self.fc2 = nn.Linear(50, num_classes)
@@ -21,13 +21,34 @@ class NN(nn.Module):
         return x
 
 
-model = NN(784, 10)
-x = torch.rand(64, 784)
-print(model(x).shape)
+# %%%%%%%%%%%%%%%%% SET THE DEVICE %%%%%%%%%%%%%%%
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# %%%%%%%%%%%%%%%%% HYPERPARAMETERS %%%%%%%%%%%%%%%
+input_size = 784
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 5
 
-# %%%%%%%%%%%%%%%%% CREATING FULLY CONNECTED NEURAL NETWORKS %%%%%%%%%%%%%%%
+# %%%%%%%%%%%%%%%%% LOADING DATA %%%%%%%%%%%%%%%
+train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-# %%%%%%%%%%%%%%%%% CREATING FULLY CONNECTED NEURAL NETWORKS %%%%%%%%%%%%%%%
+tes_dataset = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
+test_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-# %%%%%%%%%%%%%%%%% CREATING FULLY CONNECTED NEURAL NETWORKS %%%%%%%%%%%%%%%
+# %%%%%%%%%%%%%%%%% INITIALISING THE NETWORK %%%%%%%%%%%%%%%
+model = NN(input_size=input_size, num_classes=num_classes).to(device)
+
+# %%%%%%%%%%%%%%%%% TRAINING NEURAL NETWORKS %%%%%%%%%%%%%%%
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# %%%%%%%%%%%%%%%%% TRAINING NEURAL NETWORKS %%%%%%%%%%%%%%%
+for epoch in range(num_epochs):
+    for batch_idx, (data,targets) in enumerate(train_loader):
+        data = data.to(device=device)
+        targets = targets.to(device=device)
+
+        print(data.shape)
