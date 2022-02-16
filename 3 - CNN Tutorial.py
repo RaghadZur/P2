b99f90aa1fbfd812f7e3cbc5b8ecd73a59067063
Raghad <72172564+RaghadZur@users.noreply.github.com>
2022-02-16 00:24:22 +0000

    lesson 3 - cnn

--- 3 - CNN Tutorial.py
+++ 3 - CNN Tutorial.py
@@ -4,12 +4,13 @@ SIMPLE CONVOLUTIONAL NERUAL NETWORK CODE USING PYTORCH LIBRARY
 
 # %%%%%%%%%%%%%%%%%%%% IMPORTING THE PACKAGES %%%%%%%%%%%%%%%%%%%%
 import torch
-import torch.nn as nn
-import torch.optim as optim
+import torchvision
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+from torch import optim
+from torch import nn
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 
@@ -60,11 +61,11 @@ num_epochs = 5
 train_dataset = datasets.MNIST(root='dataset/', train=True, transform=transforms.ToTensor(), download=True)
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
-tes_dataset = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
-test_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_dataset = datasets.MNIST(root='dataset/', train=False, transform=transforms.ToTensor(), download=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
 # %%%%%%%%%%%%%%%%% INITIALISING THE NETWORK %%%%%%%%%%%%%%%
-model = CNN().to(device)
+model = CNN(in_channels=in_channels, num_classes=num_classes).to(device)
 
 # %%%%%%%%%%%%%%%%% TRAINING NEURAL NETWORKS %%%%%%%%%%%%%%%
 criterion = nn.CrossEntropyLoss()
@@ -72,7 +73,7 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 # %%%%%%%%%%%%%%%%% TRAINING NEURAL NETWORKS %%%%%%%%%%%%%%%
 for epoch in range(num_epochs):
-    for batch_idx, (data, targets) in enumerate(train_loader):
+    for batch_idx, (data, targets) in enumerate(tqdm(train_loader)):
         # getting data to cuda
         data = data.to(device=device)
         targets = targets.to(device=device)
@@ -99,7 +100,6 @@ def check_accuracy(loader, model):
         for x, y in loader:
             x = x.to(device=device)
             y = y.to(device=device)
-            x = x.reshape(x.shape[0], -1)
 
             scores = model(x)
             _, predictions = scores.max(1)
